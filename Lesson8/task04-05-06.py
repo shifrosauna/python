8cd938ecec70543ae5a0687580cf0d910e25127d
Egor <kravchenko_e_s@mail.ru>
2021-04-27 13:02:05 +0300

    Final

--- Lesson8/task04-05-06.py
+++ Lesson8/task04-05-06.py
@@ -12,8 +12,26 @@
 # Например, для указания количества принтеров, отправленных на склад, нельзя использовать строковый тип данных.
 # Подсказка: постарайтесь по возможности реализовать в проекте «Склад оргтехники» максимум возможностей,
 # изученных на уроках по ООП.
+
 class Storage:
-    pass
+    def __init__(self):
+        self.storage = []
+        self.dict_departments = {}
+
+    def add_equipment(self, item: "OfficeEquipment", amount: int):
+        self.storage.append([item, amount])
+
+    def transfer_to_department(self, id: int, department_name: str):
+        self.dict_departments.update({department_name: self.storage[id][0]})
+        self.storage[id][1] -= 1
+
+    @staticmethod
+    def ValidateAmount(amount):
+        try:
+            int(amount)
+        except ValueError:
+            print('Количество оргтехники может быть только целое число!')
+            exit(1)
 
 
 class OfficeEquipment:
@@ -34,34 +52,57 @@ class OfficeEquipment:
 
 
 class Printer(OfficeEquipment):
-    print_speed: int
 
-    def __init__(self, *args):
+    def __init__(self, *args, print_speed: int):
         super().__init__('Принтер', *args)
+        self.print_speed = print_speed
+
+    @property
+    def info(self):
+        return f"{self.type_equipment} {self.vendor} {self.model} {self.color} {self.price} {self.print_speed}"
 
 
 class Scanner(OfficeEquipment):
-    scan_resolution: int
 
-    def __init__(self, *args):
+    def __init__(self, *args, scan_resolution: int):
         super().__init__('Сканер', *args)
+        self.scan_resolution = scan_resolution
+
+    @property
+    def info(self):
+        return f"{self.type_equipment} {self.vendor} {self.model} {self.color} {self.price} {self.scan_resolution}"
 
 
 class Xerox(OfficeEquipment):
-    copy_speed: int
 
-    def __init__(self, *args):
+    def __init__(self, *args, copy_speed: int):
         super().__init__('Ксерокс', *args)
+        self.copy_speed = copy_speed
+
+    @property
+    def info(self):
+        return f"{self.type_equipment} {self.vendor} {self.model} {self.color} {self.price} {self.copy_speed}"
 
 
-printer = Printer("HP", "TR-500", "Белый", 2000)
-printer.print_speed = 500
-print(printer.type_equipment, printer.print_speed)
+printer = Printer("HP", "TR-500", "Белый", 2000, print_speed=500)
+scanner = Scanner("Samsung", "T-200", "Черный", 5000, scan_resolution=900)
+xerox = Xerox("Cannon", "HDJ-64", "Серый", 8000, copy_speed=1200)
 
-scanner = Scanner("Samsung", "T-200", "Черный", 5000)
-scanner.scan_resolution = 900
-print(scanner.type_equipment, scanner.scan_resolution)
+my_storage = Storage()
+# printer_amount = 'dscds'
+printer_amount = 2
+Storage.ValidateAmount(printer_amount)
+my_storage.add_equipment(printer, printer_amount)
 
-xerox = Xerox("Cannon", "HDJ-64", "Серый", 8000)
-scanner.scan_resolution = 900
-print(scanner.type_equipment, scanner.scan_resolution)
+scanner_amount = 3
+Storage.ValidateAmount(scanner_amount)
+my_storage.add_equipment(scanner, scanner_amount)
+
+xerox_amount = 5
+Storage.ValidateAmount(xerox_amount)
+my_storage.add_equipment(xerox, xerox_amount)
+
+print(my_storage.storage)
+my_storage.transfer_to_department(2, "АХО")
+print(my_storage.storage)
+print(my_storage.dict_departments)
